cbdf6096212e9bf61011aeeffdcccd6a37f84154
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-09-01 21:21:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,22 @@ provider "aws" {
     region = "us-east-1"  
 }
 
+# Create AWS EC2
 resource "aws_instance" "foo" {
   ami           = "ami-05fa00d4c63e32376" # us-west-2
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
   }
+}
+
+# Create AWS S3
+resource "aws_s3_bucket" "buck1" {
+    bucket = "mybucket43""
+    acl = "private"
+
+    tags = {
+        Name = "My bucket"
+        Environment = "DevKJT"
+    }
 }
