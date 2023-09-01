98802ed9bf00df5d3720a3d6b1c6a7b67564b86e
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-09-01 22:36:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,22 +2,27 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-# Create AWS EC2
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "bucket_name" {
+  type    = string
+  default = "my-terraform-bucket"
+}
+
+# EC2 Instance
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  ami           = "ami-05fa00d4c63e32376" # us-east-1
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
   }
 }
 
-# Create AWS S3
-resource "aws_s3_bucket" "buck1" {
-    bucket = "mybucket43""
-    acl = "private"
-
-    tags = {
-        Name = "My bucket"
-        Environment = "DevKJT"
-    }
+# S3 Bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+  acl    = "private"
 }
